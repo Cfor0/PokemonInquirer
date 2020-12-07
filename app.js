dd76cc1534d6dd6cca6a27c6a8893c072965d18f
cfor0 <cfor0@outlook.com>
2020-12-07 15:00:55 -0500

    finished battle function

--- app.js
+++ app.js
@@ -43,7 +43,35 @@ const playGame = () => {
 
                     const battleSequence = (pokemon_hp, cat_hp, pokemonName) => {
 
-                    }
+                        inquirer
+                            .prompt([
+                                {
+                                    type: 'list',
+                                    message: 'Which move will you attack with?',
+                                    choices: ['Tackle', 'Sand Attack', 'Glare'],
+                                    name: 'attack'
+                                }
+                            ])
+                            .then(res => {
+                                pokemon_hp -= Math.floor(Math.random() * 10);
+                                cat_hp -= Math.floor(Math.random() * 10);
+                                
+                                console.log('=============================================')
+                                console.log(`${pokemonName}, used ${res.attack}`);
+                                console.log(`Caterpie has ${cat_hp} health points remaining.`);
+                                console.log(`Caterpie used Tackle`);
+                                console.log(`${pokemonName}, has ${pokemon_hp} health points remaining.`);
+
+                                if (pokemon_hp <= 0) {
+                                    console.log(`${pokemonName} has fainted.`)
+                                } else if (cat_hp <= 0) {
+                                    console.log(`Caterpie has fainted, you won!`)
+                                } else {
+                                    battleSequence(pokemon_hp, cat_hp, pokemonName);
+                                }
+                            })
+                    };
+                    battleSequence(pokemon_hp, cat_hp, pokemonName);
                 })
         })
 }
